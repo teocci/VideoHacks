b451ac7e5a08da134c30566cbf2017e3b18b77b1
Teocci Kseek-PC-Ubuntu <teocci@yandex.com>
2017-04-12 11:20:48 +0900

    moving activities to the ui packet

---
+++ app/src/main/java/com/android/videohacks/interfaces/Content.java
@@ -0,0 +1,9 @@
+package com.android.videohacks.interfaces;
+
+/**
+ * Content, managed by ContentManager.
+ * TODO: re-evaluate whether this is useful
+ */
+public interface Content {
+//    File getFile();
+}
